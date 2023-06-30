3d468a3dd90d09bbe8606941c78666a7c9dd37fc
JustRevDice <93582017+JustRevDice@users.noreply.github.com>
2023-06-30 21:50:27 +0200

    I'm out of wordplay for this

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -1,7 +1,7 @@
 import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
+// see https://github.com/JustRevDice/whosjoe-joerepo
 public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
@@ -23,13 +23,15 @@ public class PrimeCheckTest {
   @Test
   public void testPrimes() {
     /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 607}
+    for (int i : primes)
+      assertEquals("Test by Primes for Primes", true, PrimeCheck.isPrime(i);
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
-  }
+    for (int i = 2; i < 50; i*=(i+1))
+      assertEquals("Test by non-Primes against Primes (don't worry about it)", false, PrimeCheck.isPrime(i));
+    }
 }
