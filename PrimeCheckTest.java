2ba45f3d7a549e7bb7c82874e2fe43c0c22058c9
JustRevDice <93582017+JustRevDice@users.noreply.github.com>
2023-06-30 21:52:21 +0200

    Bigger test ranges, syntax fixes, and I'm still out of wordplay

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -25,13 +25,13 @@ public class PrimeCheckTest {
     /* ToDo: insert a test here */
     int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 607}
     for (int i : primes)
-      assertEquals("Test by Primes for Primes", true, PrimeCheck.isPrime(i);
+      assertEquals("Test by Primes for Primes", true, PrimeCheck.isPrime(i));
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    for (int i = 2; i < 50; i*=(i+1))
+    for (int i = 4; i < 1000; i*=(i+1))
       assertEquals("Test by non-Primes against Primes (don't worry about it)", false, PrimeCheck.isPrime(i));
     }
 }
